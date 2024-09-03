584b39e548e7b6927137cbbfa55bdf6642d89f9a
nicolasrosa <nicolas.rosa@hotmail.com>
2024-09-03 19:43:48 -0300

    [Mod] scripts added

---
+++ metric_depth/predict_cane_field_dataset_images_additional_filtered_by_gama.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+RESULTS_DIRPATH="/home/nicolas/SynologyDrive/Rota2030 - Scania/cane_field_dataset/results/"
+DEPTH_PREDICTIONS_DIRPATH="predictions/metric_depth/"
+
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_filtered_by_gama/flickr.com/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_filtered_by_gama/flickr.com/" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_filtered_by_gama/Pexels/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_filtered_by_gama/Pexels" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_filtered_by_gama/pixabay/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_filtered_by_gama/pixabay" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_filtered_by_gama/rawpixel/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_filtered_by_gama/rawpixel" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_filtered_by_gama/Unsplash/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_filtered_by_gama/Unsplash" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_filtered_by_gama/Wikimedia Commons/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_filtered_by_gama/Wikimedia Commons" --input-size 1080 --pred-only --save-uint16 --max-depth 80
---
+++ metric_depth/predict_cane_field_dataset_images_additional_uploaded_on_roboflow.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+RESULTS_DIRPATH="/home/nicolas/SynologyDrive/Rota2030 - Scania/cane_field_dataset/results/"
+DEPTH_PREDICTIONS_DIRPATH="predictions/metric_depth/"
+
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_uploaded_on_roboflow/flickr.com/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_uploaded_on_roboflow/flickr.com/" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_uploaded_on_roboflow/Pexels/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_uploaded_on_roboflow/Pexels" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_uploaded_on_roboflow/pixabay/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_uploaded_on_roboflow/pixabay" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_uploaded_on_roboflow/rawpixel/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_uploaded_on_roboflow/rawpixel" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_uploaded_on_roboflow/Unsplash/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_uploaded_on_roboflow/Unsplash" --input-size 1080 --pred-only --save-uint16 --max-depth 80
+python3 run.py --encoder vitl --img-path "${RESULTS_DIRPATH}public_domain/images_additional_uploaded_on_roboflow/Wikimedia Commons/" --outdir "${DEPTH_PREDICTIONS_DIRPATH}cane_field_dataset_images_additional_uploaded_on_roboflow/Wikimedia Commons" --input-size 1080 --pred-only --save-uint16 --max-depth 80
