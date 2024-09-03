343cf2a6b77a6ee15fe9f9db40ec89355cf6a458
nicolasrosa <nicolas.rosa@hotmail.com>
2024-09-03 17:07:04 -0300

    [Mod] files removed

--- predict_lasi_images.sh
+++
@@ -1,1 +0,0 @@
-python run.py --encoder vitl --img-path /home/nicolas/Downloads/cane_field_dataset/auto_annotation/depth_estimation_annotations/Depth-Anything-V2/lasi_images --outdir predictions/lasi_images/inv_depth/ --input-size 1080 --pred-only --save-uint16
