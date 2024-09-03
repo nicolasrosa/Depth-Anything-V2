81bc7e3f312d3070a360ddfe5b9f577853b24262
nicolasrosa <nicolas.rosa@hotmail.com>
2024-09-03 15:20:33 -0300

    [Mod] install_dependencies.sh updated

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,4 @@
-pip install xformers
+pip install -U pip
+pip install -r requirements.txt
+#pip install xformers
+#pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
